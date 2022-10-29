0873fd47b1613fc18aba7dd2d0aaac6bbb2c0da4
vladch11 <craiser1@hotmail.co.il>
2022-10-29 13:46:59 +0300

    Add routing

---
+++ newamazona/frontend/src/screens/ProductScreen.js
@@ -0,0 +1,10 @@
+import { useParams } from 'react-router-dom';
+
+function ProductScreen() {
+  const params = useParams();
+  const { slug } = params;
+
+  return <div>{slug}</div>;
+}
+
+export default ProductScreen;
