bbc170da81d17daa32600d39a7074655fea2daf6
vladch11 <craiser1@hotmail.co.il>
2022-11-01 17:23:49 +0200

    Create Loading and Message Component

--- newamazona/frontend/src/screens/HomeScreen.js
+++ newamazona/frontend/src/screens/HomeScreen.js
@@ -5,6 +5,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -45,14 +47,14 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>Amazona</title>
+        <title>Amazzona</title>
       </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
-          <div>Loading...</div>
+          <LoadingBox />
         ) : error ? (
-          <div>{error}</div>
+          <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <Row>
             {products.map((product) => (
--- newamazona/frontend/src/screens/ProductScreen.js
+++ newamazona/frontend/src/screens/ProductScreen.js
@@ -9,6 +9,9 @@ import Badge from 'react-bootstrap/Badge';
 import Rating from '../components/Rating';
 import Button from 'react-bootstrap/esm/Button';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { getError } from '../utils';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -43,16 +46,16 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
     fetchData();
   }, [slug]);
 
   return loading ? (
-    <div>Loading...</div>
+    <LoadingBox />
   ) : error ? (
-    <div> {error}</div>
+    <MessageBox variant="danger">{error}</MessageBox>
   ) : (
     <div>
       <Row>
@@ -81,7 +84,14 @@ function ProductScreen() {
             </ListGroup.Item>
 
             <ListGroup.Item>
-              <b>Brand:</b> <p>{product.brand}</p>
+              <Row>
+                <Col>
+                  <b>Brand:</b> <p>{product.brand}</p>
+                </Col>
+                <Col>
+                  <b>Gender:</b> <p>{product.gender}</p>
+                </Col>
+              </Row>
             </ListGroup.Item>
 
             <ListGroup.Item>
