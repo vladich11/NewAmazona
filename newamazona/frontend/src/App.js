b6daee096500d8018fb13cef715a887527b669b6
vladch11 <craiser1@hotmail.co.il>
2022-11-19 12:29:32 +0200

    Create Signin Screen

--- newamazona/frontend/src/App.js
+++ newamazona/frontend/src/App.js
@@ -9,6 +9,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Store } from './Store';
 import { useContext } from 'react';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   // Get context
@@ -42,6 +43,7 @@ function App() {
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
